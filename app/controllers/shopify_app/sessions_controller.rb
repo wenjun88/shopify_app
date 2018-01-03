28e50591d9d2fc69c87384c6e5883792f8f05cf7
Patrick Fang <patrick.fang@shopify.com>
2018-01-03 16:44:25 -0500

    update authenticate method to include origin value

--- app/controllers/shopify_app/sessions_controller.rb
+++ app/controllers/shopify_app/sessions_controller.rb
@@ -38,8 +38,10 @@ module ShopifyApp
 
     def authenticate
       if sanitized_shop_name.present?
+        omniauth_oauth_url = "#{main_app.root_path}auth/shopify?shop=#{sanitized_shop_name}"
+        omniauth_oauth_url += "&origin=#{CGI::escape origin}" if origin.present?
         session['shopify.omniauth_params'] = { shop: sanitized_shop_name }
-        fullpage_redirect_to "#{main_app.root_path}auth/shopify"
+        fullpage_redirect_to omniauth_oauth_url
       else
         flash[:error] = I18n.t('invalid_shop_url')
         redirect_to return_address
